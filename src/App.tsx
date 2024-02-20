7bd514a1b0f837b6bd9f960c16c0a7bd1ed38728
milanasolyanka <tupeykoma@gmail.com>
2024-02-21 04:38:09 +0700

    added scroll to the hash element after page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 import Header from "components/Header";
 import Footer from "components/Footer";
 import Divider from "components/Divider";
@@ -11,6 +14,23 @@ import ContactPage from "pages/ContactPage";
 import "./App.scss";
 
 function App() {
+  // код для прокрутки до якоря при перезагрузке страницы
+  const location = useLocation();
+  const [hash, setHash] = useState<string>("");
+
+  useEffect(() => {
+    if (location.hash) {
+      setHash(location.hash.slice(1));
+      if (hash && document.getElementById(hash)) {
+        setTimeout(() => {
+          document
+            .getElementById(hash)
+            ?.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 300);
+      }
+    }
+  }, [hash]);
+
   return (
     <div className="App">
       <Header />
