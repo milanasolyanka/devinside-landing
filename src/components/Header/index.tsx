e160e138a3ffdc8a9a971d4c7b16137de3c5c517
milanasolyanka <tupeykoma@gmail.com>
2024-02-23 03:59:32 +0700

    adapted navbar for mobiles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,7 @@ import CustomNavLink from "components/UI/CustomNavLink";
 import headerData from "data/headerData";
 
 import "./header.scss";
+import "./header-media.scss";
 
 const Header = () => {
   // код для цвета хэдера - белый или прозрачный
@@ -23,15 +24,51 @@ const Header = () => {
     });
   }, []);
 
+  // код для адаптива, для кнопочки показа sidebar
+  const [sidebarVisibility, setSidebarVisibility] =
+    useState<string>("sidebar-invisible");
+  const sidebarToggler = () => {
+    if (sidebarVisibility === "sidebar-invisible")
+      setSidebarVisibility("sidebar-visible");
+    if (sidebarVisibility === "sidebar-visible")
+      setSidebarVisibility("sidebar-invisible");
+  };
+
   return (
     <div className={`header ${headerColor}`}>
+      <button className="sidebar-toggler" onClick={sidebarToggler}>
+        {sidebarVisibility === "sidebar-invisible" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="30"
+            viewBox="0 -960 960 960"
+            width="30"
+            fill="white"
+          >
+            <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="30"
+            viewBox="0 -960 960 960"
+            width="30"
+            fill="white"
+          >
+            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+          </svg>
+        )}
+      </button>
       <div className="header__holder">
         <div className="header__logo">
           <Link to="/" reloadDocument>
             DEVINSIDE
           </Link>
         </div>
-        <div className="header__menu">
+        <div
+          className={`header__menu ${sidebarVisibility}`}
+          onClick={(e) => setSidebarVisibility("sidebar-invisible")}
+        >
           {headerData.map((item) => (
             <CustomNavLink
               key={item.link}
